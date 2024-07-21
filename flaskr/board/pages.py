1db3883cf2ea1d52901eb760f193af952083eacc
nasirthomasit <nasirthomasit@gmail.com>
2024-07-21 15:06:38 -0400

    Added search functionality

--- flaskr/board/pages.py
+++ flaskr/board/pages.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, current_app, g
+from flask import Blueprint, render_template, current_app, request
 
 import sys
 import os
@@ -14,19 +14,72 @@ from flask_mysqldb import MySQL
 
 bp = Blueprint("pages", __name__)
 
+@bp.route('/index')
+def index():
+    return render_template('pages/index.html')
 
-@bp.route('/')
-def home():
+@bp.route('/search', methods=['POST'])
+def search():
+    query = request.form.get('query')
     try:
         mysql = current_app.mysql
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM poke_stats ;")
+        cursor.execute("SELECT * FROM poke_stats Where poke_name LIKE %s;", (f"%{query}%",))
         rows = cursor.fetchall()
         cursor.close()
         #print(rows)
         return render_template('pages/home.html', rows=rows)
     except Exception as e:
         logging.error(f"could not insert data due to {e}")
+        return render_template('pages/home.html', rows=[])
+    
+
+@bp.route('/search/type', methods=['POST'])
+def search_type():
+    query = request.form.get('query')
+    try:
+        mysql = current_app.mysql
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM poke_stats Where type LIKE %s;", (f"%{query}%",))
+        rows = cursor.fetchall()
+        cursor.close()
+        #print(rows)
+        return render_template('pages/home.html', rows=rows)
+    except Exception as e:
+        logging.error(f"could not insert data due to {e}")
+        return render_template('pages/home.html', rows=[])
+
+
+@bp.route('/search/ability', methods=['POST'])
+def search_ability():
+    query = request.form.get('query')
+    try:
+        mysql = current_app.mysql
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM poke_stats Where ability LIKE %s;", (f"%{query}%",))
+        rows = cursor.fetchall()
+        cursor.close()
+        #print(rows)
+        return render_template('pages/home.html', rows=rows)
+    except Exception as e:
+        logging.error(f"could not insert data due to {e}")
+        return render_template('pages/home.html', rows=[])
+
+
+@bp.route('/')
+def home():
+    try:
+        mysql = current_app.mysql
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM poke_stats;")
+        rows = cursor.fetchall()
+        cursor.close()
+        #print(rows)
+        return render_template('pages/home.html', rows=rows)
+    except Exception as e:
+        logging.error(f"could not insert data due to {e}")
+        return render_template('pages/home.html', rows=[])
+
 
 @bp.route("/about")
 def about():
